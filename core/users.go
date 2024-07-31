37cc6c2a9b9a79b421f6cabd76fdad9f3da3dfcb
Robin Arys <robin@arys.dev>
2024-07-31 17:22:40 +0200

    Add a parse user id function

--- core/users.go
+++ core/users.go
@@ -2,6 +2,8 @@ package core
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
 	"time"
 )
 
@@ -22,4 +24,20 @@ func NewUserID(id uint32) (UserID, error) {
 		return 0, errors.New("UserID cannot be 0")
 	}
 	return UserID(id), nil
+}
+
+// ParseUserID parses a string into a user id.
+func ParseUserID(id string) (UserID, error) {
+	integerID, err := strconv.Atoi(id)
+	if err != nil {
+		return 0, fmt.Errorf("cannot parse user id: %w", err)
+	}
+	if integerID < 0 {
+		return 0, errors.New("cannot parse user id: user ids cannot be negative")
+	}
+	userID, err := NewUserID(uint32(integerID))
+	if err != nil {
+		return 0, fmt.Errorf("cannot parse user id: %w", err)
+	}
+	return userID, nil
 }
