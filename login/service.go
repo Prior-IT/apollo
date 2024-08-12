ce7d684954dfb150b7a0521b6db125ca2763b068
Robin Arys <robin@arys.dev>
2024-08-12 15:05:59 +0200

    Update doc comment

--- login/service.go
+++ login/service.go
@@ -6,7 +6,8 @@ type UserData struct {
 	Name  string `form:"name"        json:"name"`
 	Email string `form:"email"       json:"email"`
 	// Either an OAuth provider or the login method
-	Provider   string `form:"provider"    json:"provider"`
+	Provider string `form:"provider"    json:"provider"`
+	// External user id, currently only used with OAuth
 	ProviderID string `form:"provider_id" json:"provider_id"`
 }
 
@@ -15,7 +16,8 @@ type Service interface {
 	// Only used in login services that require the user to initiate login (e.g. OAuth)
 	GetLoginRedirectURL(provider string, callbackURL string) (string, error)
 
-	// Handle the callback from a login server.
+	// Handle the callback from a login server. The returned UserData might be incomplete if this is a new user and
+	// not all data could be retrieved from the chosen provider.
 	LoginCallback(
 		ctx context.Context,
 		provider string,
