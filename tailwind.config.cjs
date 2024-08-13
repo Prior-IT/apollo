45a100972b7d31fde2f474a22031abbf2ab18600
Robin Arys <robin@arys.dev>
2024-08-13 15:51:21 +0200

    Make sure tailwind actually generates placeholders for primary- and secondary colours

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["**/*.templ"],
-	theme: {},
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					light: "#DF504B",
+					DEFAULT: "#AD2831",
+					dark: "#800E13",
+				},
+				secondary: {
+					light: "#E9F0FA",
+					DEFAULT: "#245ED6",
+					dark: "#0D41AA",
+				},
+			},
+		},
+	},
 	plugins: [],
 };
