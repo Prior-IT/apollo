3b21a450241f2fd1060fbdf9d2644e61d095a18f
Wouter Baes <wouter.baes@prior-it.be>
2024-08-16 14:21:20 +0200

    Add error type

--- core/errors.go
+++ core/errors.go
@@ -3,9 +3,10 @@ package core
 import "errors"
 
 var (
-	ErrUserDoesNotExist = errors.New("user does not exist")
-	ErrNotFound         = errors.New("not found")
-	ErrUnauthenticated  = errors.New("authentication required")
-	ErrForbidden        = errors.New("user is not authorized")
-	ErrConflict         = errors.New("conflict")
+	ErrUserDoesNotExist         = errors.New("user does not exist")
+	ErrOrganisationDoesNotExist = errors.New("organisation does not exist")
+	ErrNotFound                 = errors.New("not found")
+	ErrUnauthenticated          = errors.New("authentication required")
+	ErrForbidden                = errors.New("user is not authorized")
+	ErrConflict                 = errors.New("conflict")
 )
