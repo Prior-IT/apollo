f9dae44fc8029c012b628a6a7ffac27ccb6663ca
Wouter Baes <wouter.baes@prior-it.be>
2024-08-16 16:35:31 +0200

    Remove pointers for lists

--- core/organisations.go
+++ core/organisations.go
@@ -62,15 +62,15 @@ type OrganisationService interface {
 	// Retrieve the organisation with the specified id or ErrOrganisationDoesNotExist if no such organisation exists.
 	GetOrganisation(ctx context.Context, id OrganisationID) (*Organisation, error)
 	// Retrieve all existing organisations.
-	ListOrganisations(ctx context.Context) ([]*Organisation, error)
+	ListOrganisations(ctx context.Context) ([]Organisation, error)
 	// Retrieve the amount of existing organisations.
 	GetAmountOfOrganisations(ctx context.Context) (uint64, error)
 	// Delete the organisation with the specified id or ErrOrganisationDoesNotExist if no such organisation exists.
 	DeleteOrganisation(ctx context.Context, id OrganisationID) error
 	// List the organisations a user belongs to or ErrUserDoesNotExist if no such user exists
-	ListOrganisationsForUser(ctx context.Context, id UserID) ([]*Organisation, error)
+	ListOrganisationsForUser(ctx context.Context, id UserID) ([]Organisation, error)
 	// List the users that belong to an organisation or ErrOrganisationDoesNotExist if no such organisation exists
-	ListUsersinOrganisation(ctx context.Context, id OrganisationID) ([]*User, error)
+	ListUsersinOrganisation(ctx context.Context, id OrganisationID) ([]User, error)
 	// Add user to an existing organisation
 	AddUserToOrganisation(ctx context.Context, UserID UserID, OrgID OrganisationID) (*Organisation, error)
 	// Remove user from an organisation
