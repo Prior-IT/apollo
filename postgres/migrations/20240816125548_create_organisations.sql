74a24609b89e4ed30b7f717afe50511433fb1a14
Robin Arys <robin@arys.dev>
2024-08-17 07:39:26 +0200

    Add organisation_users index to make query more efficient

--- postgres/migrations/20240816125548_create_organisations.sql
+++ postgres/migrations/20240816125548_create_organisations.sql
@@ -1,20 +1,29 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS apollo.organisations (
-	id serial PRIMARY KEY,
-	name text NOT NULL
+    id serial NOT NULL,
+    name text NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS apollo.organisation_users (
-	id serial PRIMARY KEY,
-	user_id serial NOT NULL REFERENCES apollo.users(id) ON DELETE CASCADE,
-	organisation_id serial NOT NULL REFERENCES apollo.organisations(id) ON DELETE CASCADE,
-	UNIQUE (user_id, organisation_id)
+    id serial NOT NULL,
+    user_id serial NOT NULL,
+    organisation_id serial NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES apollo.users (id) ON DELETE CASCADE,
+    FOREIGN KEY (organisation_id) REFERENCES apollo.organisations (id) ON DELETE CASCADE
 );
--- +goose StatementEnd
 
+CREATE UNIQUE INDEX unique_organisation_id_users_id_idx ON apollo.organisation_users (user_id, organisation_id);
+
+-- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
+DROP INDEX IF EXISTS unique_organisation_id_users_id_idx;
+
 DROP TABLE IF EXISTS apollo.organisation_users;
+
 DROP TABLE IF EXISTS apollo.organisations;
+
 -- +goose StatementEnd
